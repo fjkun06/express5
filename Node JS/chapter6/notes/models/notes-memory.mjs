c02fe552bf0f4157b5e38f94464257553bcc76d6
fjkun06 <fjkun06@gmail.com>
2023-01-29 19:16:32 +0100

    logging

---
+++ Node JS/chapter6/notes/models/notes-memory.mjs
@@ -0,0 +1,49 @@
+import { AbstractNotesStore, Note } from "./Notes.mjs";
+
+const notes = [];
+notes["schneider"] = new Note("schneider", "Schneider", "She a real vixen and she is in Mamfe rn.");
+notes["nance"] = new Note("nance", "Nance", "She a real troublemaker and she is in a hospital rn.");
+
+export class InMemoryNotesstore extends AbstractNotesStore {
+  async close() {}
+
+  async update(key, title, body) {
+    notes[key] = new Note(key, title, body);
+    // console.log(notes);
+    return notes[key];
+  }
+  async create(key, title, body) {
+    notes[key] = new Note(key, title, body);
+    // console.log(notes);
+    return notes[key];
+  }
+  async copy() {
+    return notes;
+    // return notes['nance'];
+  }
+  async read(key) {
+    // console.log(notes);
+    if (notes[key]) return notes[key];
+    else throw new Error(`Note ${key} doesn't exist`);
+  }
+
+  async destroy(key) {
+    console.log("notes: ", notes[key]);
+    console.log("notes2: ", key);
+    console.log("notes3: ", notes);
+    if (notes[key]) {
+      delete notes[key];
+    } else {
+      throw new Error(`Note ${key} doesn't exist`);
+    }
+  }
+
+  async keylist() {
+    // console.log(notes);
+    return Object.keys(notes);
+  }
+
+  async count() {
+    return notes.length;
+  }
+}
---
+++ Node JS/chapter6/notes/routes/notes.mjs
@@ -0,0 +1,113 @@
+// const util = require('util');
+import { default as express } from "express";
+import { NotesStore as notes } from "../app.mjs";
+
+export const router = express.Router();
+
+//Add Note
+router.get("/add", (req, res, next) => {
+  // res.send("hello world note!");
+  res.render("noteedit", { title: "Add a Note", docreate: true, notekey: "", note: undefined });
+});
+
+//Save Note
+router.post("/save", async (req, res, next) => {
+  // res.send("hello save note!");
+
+  try {
+    let note;
+    if (req.body.docreate === "create") {
+      await notes.create(req.body.notekey, req.body.title, req.body.body);
+      // note = await notes.create(req.body.notekey, req.body.title, req.body.body);
+    } else {
+      await notes.update(req.body.notekey, req.body.title, req.body.body);
+      // note = await notes.update(req.body.notekey, req.body.title, req.body.body);
+    }
+    // res.redirect("http://localhost:3000");
+    res.redirect("/notes/view?key=" + req.body.notekey);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//Read Note
+router.get("/view", async (req, res, next) => {
+  try {
+    let note = await notes.read(req.query.key);
+    res.render("noteview", {
+      title: note ? note.title : "",
+      notekey: req.query.key,
+      note: note,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+//Edit note (update)
+router.get("/edit", async (req, res, next) => {
+  try {
+    const note = await notes.read(req.query.key);
+    res.render("noteedit", {
+      title: note ? "Edit " + note.title : "Add a Note",
+      docreate: false,
+      notekey: req.query.key,
+      note: note,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+//Send note (data)
+router.get("/data", async (req, res, next) => {
+  // const note = await notes.read(req.query.key);
+  try {
+    const keylist = await notes.keylist();
+    // console.log(`keylist ${util.inspect(keylist)}`);
+    const keyPromises = keylist.map(async(key) => {
+      const note = await notes.read(key)
+      return note;
+    });
+    const notelist = await Promise.all(keyPromises);
+    // console.log(util.inspect(notelist));
+
+    // res.render("index", { title: "Notes", notelist: notelist });
+
+    if (notelist.length > 0) {
+      // if (notelist.length > 0 && Object.keys(notelist[0]).length > 0) {
+      res.send({ title: "Notes", notelist: keyPromises });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+//Delete note (destroy)
+router.get("/destroy", async (req, res, next) => {
+  try {
+    // await notes.destroy(req.query.key);
+    // res.redirect("http://localhost:3000");
+    // res.end();
+
+    const note = await notes.read(req.query.key);
+    res.render("notedestroy", {
+      title: note ? `Delete ${note.title}` : "",
+      notekey: req.query.key,
+      note: note,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Really destroy note
+router.post("/destroy/confirm", async (req, res, next) => {
+  try {
+    const key = req.body.notekey.slice(3 - req.body.notekey.length);
+    await notes.destroy(key);
+    res.redirect("/");
+    // res.send(req.body);
+  } catch (error) {
+    next(error);
+  }
+});
